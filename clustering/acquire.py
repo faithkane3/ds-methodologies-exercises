f3d934edcd3de3093cd71679de3ada946a57d861
faithkane3 <faith.x.kane@gmail.com>
2019-11-03 11:59:19 -0600

    Commit ./ and restarting computer. I don't know wht this file is, and I don't see the git tracking my other files.

---
+++ clustering/acquire.py
@@ -0,0 +1,137 @@
+import pandas as pd
+import env
+
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+# Remove any properties that are likely to be something other than a single unit properties (e.g. no duplexes, no land/lot, ...). There are multiple ways to estimate that a property is a single unit, and there is not a single "right" answer.
+
+def get_zillow_data():
+    query = '''
+    select 
+    svi.`COUNTY` county,
+    p.`taxamount`/p.`taxvaluedollarcnt` tax_rate,
+    p.`id`,
+    p.`parcelid`,
+    p.`airconditioningtypeid`,
+    act.`airconditioningdesc`,
+	p.`architecturalstyletypeid`,
+	ast.`architecturalstyledesc`,
+    p.`basementsqft`,
+	p.`bathroomcnt`,
+    p.`bedroomcnt`,
+	p.`buildingclasstypeid`,
+	bct.`buildingclassdesc`,
+    p.`buildingqualitytypeid`,
+    p.`calculatedbathnbr`,
+    p.`calculatedfinishedsquarefeet`,
+    p.`decktypeid`,
+    p.`finishedfloor1squarefeet`,
+    p.`finishedsquarefeet12`,
+    p.`finishedsquarefeet13`,
+    p.`finishedsquarefeet15`,
+    p.`finishedsquarefeet50`,
+    p.`finishedsquarefeet6`,
+    p.`fips`,
+    svi.`ST_ABBR` state,
+    p.`fireplacecnt`,
+    p.`fullbathcnt`,
+    p.`garagecarcnt`,
+    p.`garagetotalsqft`,
+    p.`hashottuborspa`,
+    p.`heatingorsystemtypeid`,
+    hst.`heatingorsystemdesc`,
+    p.`latitude`,
+    p.`longitude`,
+    p.`lotsizesquarefeet`,
+    p.`poolcnt`,
+    p.`poolsizesum`,
+    p.`pooltypeid10`,
+    p.`pooltypeid2`,
+    p.`pooltypeid7`,
+    p.`propertycountylandusecode`,
+    p.`propertylandusetypeid`,
+    plut.`propertylandusedesc`,
+    p.`propertyzoningdesc`,
+    p.`rawcensustractandblock`,
+    p.`regionidcity`,
+    p.`regionidcounty`,
+    p.`regionidneighborhood`,
+    p.`regionidzip`,
+    p.`roomcnt`,
+	p.`storytypeid`,
+	st.`storydesc`,
+    p.`taxvaluedollarcnt`,
+    p.`threequarterbathnbr`,
+    p.`unitcnt`,
+    p.`yardbuildingsqft17`,
+    p.`yardbuildingsqft26`,
+    p.`yearbuilt`,
+    p.`numberofstories`,
+    p.`fireplaceflag`,
+    p.`structuretaxvaluedollarcnt`,
+    p.`assessmentyear`,
+    p.`landtaxvaluedollarcnt`,
+    p.`taxamount`,
+    p.`taxdelinquencyflag`,
+    p.`taxdelinquencyyear`, 
+	p.`typeconstructiontypeid`,
+	tct.`typeconstructiondesc`,
+    p.`censustractandblock`,
+    pred.`transactiondate`,
+    pred.`logerror`,
+    m.`transactions`
+    from 
+	`properties_2017` p
+    inner join `predictions_2017`  pred
+    on p.`parcelid` = pred.`parcelid` 
+    inner join 
+	(select 
+		`parcelid`, 
+		max(`transactiondate`) `lasttransactiondate`, 
+		max(`id`) `maxid`, 
+		count(*) `transactions`
+	from 
+		predictions_2017
+	group by 
+		`parcelid`
+	) m
+	on 
+	pred.parcelid = m.parcelid
+	and pred.transactiondate = m.lasttransactiondate
+    left join `propertylandusetype` plut
+        on p.`propertylandusetypeid` = plut.`propertylandusetypeid`
+            
+    left join svi_db.svi2016_us_county svi
+        on p.`fips` = svi.`FIPS`
+    left join `airconditioningtype` act
+        using(`airconditioningtypeid`)
+    left join heatingorsystemtype hst
+        using(`heatingorsystemtypeid`)
+    left join `architecturalstyletype` ast
+        using(`architecturalstyletypeid`)
+    left join `buildingclasstype` bct
+        using(`buildingclasstypeid`)
+    left join `storytype` st
+        using(`storytypeid`)
+    left join `typeconstructiontype` tct
+        using(`typeconstructiontypeid`)
+    where 
+        p.`latitude` is not null
+        and p.`longitude` is not null
+        and p.bedroomcnt > 0 and p.bathroomcnt > 0
+        and plut.propertylandusetypeid = '261';
+    '''
+    return pd.read_sql(query, get_connection('zillow'))
+
+def get_iris_data():
+    query = '''
+    SELECT petal_length, petal_width, sepal_length, sepal_width, species_id, species_name
+    FROM measurements m
+    JOIN species s USING(species_id)
+    '''
+    return pd.read_sql(query, get_connection('iris_db'))
+
+def get_mallcustomer_data():
+    df = pd.read_sql('SELECT * FROM customers;', get_connection('mall_customers'))
+    return df.set_index('customer_id')
---
+++ clustering/summarize.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+def nulls_by_col(df):
+    num_missing = df.isnull().sum()
+    rows = df.shape[0]
+    pct_missing = num_missing/rows
+    cols_missing = pd.DataFrame({'num_rows_missing': num_missing, 'pct_rows_missing': pct_missing})
+    return cols_missing
+
+def nulls_by_row(df):
+    num_cols_missing = df.isnull().sum(axis=1)
+    pct_cols_missing = df.isnull().sum(axis=1)/df.shape[1]*100
+    rows_missing = pd.DataFrame({'num_cols_missing': num_cols_missing, 'pct_cols_missing': pct_cols_missing}).reset_index().groupby(['num_cols_missing','pct_cols_missing']).count().rename(index=str, columns={'index': 'num_rows'}).reset_index()
+    return rows_missing
+
+def df_value_counts(df):
+    for col in df.columns:
+        print(f'{col}:')
+        if df[col].dtype == 'object':
+            col_count = df[col].value_counts()
+        else:
+            if df[col].nunique() >= 35:
+                col_count = df[col].value_counts(bins=10, sort=False)
+            else:
+                col_count = df[col].value_counts()
+        print(col_count)
+
+def df_summary(df):
+    print('--- Shape: {}'.format(df.shape))
+    print('--- Info')
+    df.info()
+    print('--- Descriptions')
+    print(df.describe(include='all'))
+    print('--- Nulls By Column')
+    print(nulls_by_col(df))
+    print('--- Nulls By Row')
+    print(nulls_by_row(df))
+    print('--- Value Counts')
+    print(df_value_counts(df))
