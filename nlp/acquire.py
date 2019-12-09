c60326d5ac2574a767af55e7787da3c40531d214
faithkane3 <faith.x.kane@gmail.com>
2019-12-09 15:29:54 -0600

    Commit updated modules for nlp pipeline

--- nlp/acquire.py
+++ nlp/acquire.py
@@ -3,6 +3,15 @@ from bs4 import BeautifulSoup
 import os
 import pandas as pd
 
+def get_blog_posts():
+    filename = './codeup_blog_posts.csv'
+
+    # check for presence of the file or make a new request
+    if os.path.exists(filename):
+        return pd.read_csv(filename)
+    else:
+        return make_new_request()
+
 def make_dictionary_from_article(url):
     headers = {'User-Agent': 'Codeup Bayes Data Science'}
     response = get(url, headers=headers)
@@ -16,7 +25,7 @@ def make_dictionary_from_article(url):
     
     return output
 
-def get_blog_articles():
+def make_new_request():
     urls = ['https://codeup.com/codeups-data-science-career-accelerator-is-here/',
             'https://codeup.com/data-science-myths/',
             'https://codeup.com/data-science-vs-data-analytics-whats-the-difference/',
@@ -44,3 +53,61 @@ def get_article_text():
     else:
         return get_blog_articles()
 
+# Function to get news articles
+
+def get_news_articles():
+    filename = 'inshorts_news_articles.csv'
+
+    # check for presence of the file or make a new request
+    if os.path.exists(filename):
+        return pd.read_csv(filename)
+    else:
+        return make_new_request()
+
+def get_articles_from_topic(url):
+    headers = {'user-agent': 'Codeup Bayes Instructor Example'}
+    response = get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    output = []
+
+    articles = soup.select(".news-card")
+
+    for article in articles: 
+        title = article.select("[itemprop='headline']")[0].get_text()
+        body = article.select("[itemprop='articleBody']")[0].get_text()
+        author = article.select(".author")[0].get_text()
+        published_date = article.select(".time")[0]["content"]
+        category = response.url.split("/")[-1]
+
+        article_data = {
+            'title': title,
+            'body': body,
+            'category': category,
+            'author': author,
+            'published_date': published_date,
+        }
+        output.append(article_data)
+
+
+    return output
+
+
+def make_new_request():
+    urls = [
+        "https://inshorts.com/en/read/business",
+        "https://inshorts.com/en/read/sports",
+        "https://inshorts.com/en/read/technology",
+        "https://inshorts.com/en/read/entertainment"
+    ]
+
+    output = []
+    
+    for url in urls:
+        # We use .extend in order to make a flat output list.
+        output.extend(get_articles_from_topic(url))
+
+    df = pd.DataFrame(output)
+    df.to_csv('inshorts_news_articles.csv') 
+
+    return df
--- nlp/prepare.py
+++ nlp/prepare.py
@@ -18,35 +18,35 @@ def normalize(string):
 def remove_special_characters(string):
     return re.sub(r"[^a-z0-9'\s]", '', string)
 
-def basic_clean(s):
-    s = s.lower()
-    s = s.strip()
-    s= unicodedata.normalize('NFKD', s)\
+def basic_clean(string):
+    string = string.lower()
+    string = string.strip()
+    string = unicodedata.normalize('NFKD', string)\
     .encode('ascii', 'ignore')\
     .decode('utf-8', 'ignore')
-    s = re.sub(r"[^a-z0-9'\s]", '', s)
-    return s
+    string = re.sub(r"[^a-z0-9'\s]", '', string)
+    return string
 
-def tokenize(s):
+def tokenize(string):
     tokenizer = nltk.tokenize.ToktokTokenizer()
-    return tokenizer.tokenize(s, return_str=True)
+    return tokenizer.tokenize(string, return_str=True)
 
-def stem(s):
-    ps = nltk.porter.PorterStemmer()
-    stems = [ps.stem(word) for word in s.split()]
+def stem(string):
+    ps = nltk.PorterStemmer()
+    stems = [ps.stem(word) for word in string.split()]
     string_of_stems = ' '.join(stems)
     return string_of_stems
 
-def lemmatize(s):
+def lemmatize(string):
     wnl = nltk.stem.WordNetLemmatizer()
-    lemmas = [wnl.lemmatize(word) for word in s.split()]
+    lemmas = [wnl.lemmatize(word) for word in string.split()]
     string_of_lemmas = ' '.join(lemmas)
     return string_of_lemmas
 
-def remove_stopwords(s, extra_words=[], exclude_words=[]):
-    s = tokenize(s)
+def remove_stopwords(string, extra_words=[], exclude_words=[]):
+    string = tokenize(string)
     
-    words = s.split()
+    words = string.split()
     stopword_list = stopwords.words('english')
     
     stopword_list = set(stopword_list) - set(exclude_words)
@@ -58,10 +58,36 @@ def remove_stopwords(s, extra_words=[], exclude_words=[]):
     return final_string
 
 def prep_articles(df):
-    df['original'] = df.body
-    df['stemmed'] = df.body.apply(basic_clean).apply(stem)
-    df['lemmatized'] = df.body.apply(basic_clean).apply(lemmatize)
-    df['clean'] = df.body.apply(basic_clean).apply(remove_stopwords)
-    df.drop(columns=['body'], inplace=True)
+    df = df.assign(original = df.title)
+    df = df.assign(normalized = df.original.apply(normalize))
+    df = df.assign(stemmed = df.normalized.apply(stem))
+    df = df.assign(lemmatized = df.normalized.apply(lemmatize))
+    df = df.assign(cleaned = df.stemmed.apply(remove_stopwords))
+    df.drop(columns=["title"], inplace=True)
     return df
+
+# def prep_articles(df):
+#     df['original'] = df.body
+#     df['stemmed'] = df.body.apply(basic_clean).apply(stem)
+#     df['lemmatized'] = df.body.apply(basic_clean).apply(lemmatize)
+#     df['clean'] = df.body.apply(basic_clean).apply(remove_stopwords)
+#     df.drop(columns=['body'], inplace=True)
+#     return df
+
+def prep_blog_posts():
+    df = acquire.get_blog_posts()
+    return prep_articles(df)
+
+def prep_news_articles():
+    df = acquire.get_news_articles()
+    return prep_articles(df)
+
+def prep_corpus():
+    blog_df = prep_blog_posts()
+    blog_df["source"] = "Codeup Blog"
+
+    news_df = prep_news_articles()
+    news_df["source"] = "InShorts News"
+
+    return blog_df, news_df
 
