0511bfa890202fea00c425db4ecf3cafd44cffa5
Faith Kane <faith.x.kane@gmail.com>
2020-04-13 19:31:54 -0500

    Commit work on prepare encoding function

---
+++ classification/prepare_walkthrough.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import scipy as sp 
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
+
