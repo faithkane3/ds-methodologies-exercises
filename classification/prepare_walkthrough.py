b0b5fe18e711d16bd9b6f79cac5334fcf649f30f
Faith Kane <faith.x.kane@gmail.com>
2020-04-13 23:15:52 -0500

    Commit most of prepare complete, working on simple imputer

--- classification/prepare_walkthrough.py
+++ classification/prepare_walkthrough.py
@@ -6,3 +6,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
 
+def prep_iris(df):
+    train, test = train_test_split(df, train_size=.75, random_state=123)
+    le = LabelEncoder()
+    train['species'] = le.fit_transform(train[['species']])
+    test['species'] = le.transform(test[['species']])
+    return train, test
+
+def inverse_encode(train_encoded, test_encoded):
+    train['species'] = le.inverse_transform(train.species)
+    test['species'] = le.inverse_transform(test.species)
+    return train, test
+
